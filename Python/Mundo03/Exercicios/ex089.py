68b8a20ae644abf571ce9f2ff30ea901d50b0caa
Estevam Jannuzzi <85847449+eStev4m@users.noreply.github.com>
2021-08-14 10:28:04 -0300

    Create ex089.py

---
+++ Python/Mundo03/Exercicios/ex089.py
@@ -0,0 +1,26 @@
+'''Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.'''
+
+alunos = []
+while True:
+    nome = str(input('Nome: '))
+    n1 = float(input('Nota 1: '))
+    n2 = float(input('Nota 2: '))
+    media = (n1 + n2) / 2
+    alunos.append([nome, [n1, n2], media])
+    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp in 'N':
+        break
+print('-=' * 30)
+print(f'{"Nº.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-' * 26)
+for i, a in enumerate(alunos):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(alunos) - 1:
+        print(f'Notas de {alunos[opc][0]} são {alunos[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')

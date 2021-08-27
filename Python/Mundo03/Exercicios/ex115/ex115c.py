ce27cb0f365b7b39dd91618a63aa3bfe714af439
Estevam Jannuzzi <85847449+eStev4m@users.noreply.github.com>
2021-08-27 16:19:01 -0300

    fimdemundo3

---
+++ Python/Mundo03/Exercicios/ex115/ex115c.py
@@ -0,0 +1,26 @@
+from lib.interface.menu import *
+from lib.arquivo.archive import *
+from time import sleep
+
+arq = 'Cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        # Opção de listar o conteudo de um arquivo!
+        lerArquivo(arq)
+    elif resposta == 2:
+        # Opção de cadastrar uma nova pessoa.
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: ')).strip()
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabecalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(1)

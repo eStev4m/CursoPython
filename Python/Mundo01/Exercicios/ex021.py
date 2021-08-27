0327288b5e1b544f68ccc2598300149c4cb91c97
Estevam Jannuzzi <85847449+eStev4m@users.noreply.github.com>
2021-08-26 22:12:02 -0300

    Update ex021.py

--- Python/Mundo01/Exercicios/ex021.py
+++ Python/Mundo01/Exercicios/ex021.py
@@ -1,8 +1,9 @@
 #Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 
 import pygame
+from pygame import mixer
 
 pygame.init()
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play()
+mixer.music.load("ex021.mp3")
+mixer.music.play()
 pygame.event.wait()
